003ad2a096117ac46c9df1121e09e65d2e95e942
Unknown <longtth85@gmail.com>
2017-10-15 20:24:37 +0200

    Added a mainpage with navbar

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../CSS/App.css';
-import MainPage from "../components/TodoPage";
+import TodoPage from "../components/TodoPage";
+import MainPage from "../components/MainPage";
 import DocumentationPage from "../components/DocumentationBtn";
 import { Switch, Route } from 'react-router-dom';
 
@@ -8,7 +9,7 @@ import { Switch, Route } from 'react-router-dom';
 class App extends Component {
   render() {
     return (
-         <TodoPage />
+         <MainPage />
     );
   }
 }
