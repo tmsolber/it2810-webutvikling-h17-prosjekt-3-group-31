f99dc40fbda2a83982d38d2345e038d58ba858fc
Unknown <longtth85@gmail.com>
2017-10-15 19:48:22 +0200

    Added popup text to addtodo

--- src/components/AddToDo.js
+++ src/components/AddToDo.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
 import plus from '../images/plus.png';
 import '../CSS/Buttons.css';
+import {Popup} from 'semantic-ui-react'
 
 class AddToDo extends Component{
     render() {
         return (
             <div className="add-btn">
-                <img src={plus} alt="add-to-do" className="plus" onClick={this.props.show}/>
+                <Popup
+                    trigger={<img src={plus} alt="add-to-do" className="plus" onClick={this.props.show}/>}
+                    content={'Click to add todo'}
+                />
+
             </div>
         );
     }
