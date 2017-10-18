506f6d618ddf491da6c84702481ab6d63b87bb45
krissaberg <kristinahberg95@gmail.com>
2017-10-18 21:21:23 +0200

    Tried to make a test for the Clock Container

---
+++ tests/ClockContainer.test.js
@@ -0,0 +1,14 @@
+import ClockContainer from '../containers/ClockContainer';
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import toJson from 'enzyme-to-json';
+
+configure({ adapter: new Adapter() });
+
+test('Clock component should render as expected', () =>{
+  const component = shallow(<ClockContainer />);
+  const tree = toJson(component);
+  expect(tree).toLocaleTimeString;
+});
